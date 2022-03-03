297b35c81323c43d9eff39efe587f2174eac9607
Vladislav Ozhgibesov <vozhgibesov@gmail.com>
2022-03-03 21:57:43 +0700

    Добавьте файлы проекта.

---
+++ lab3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        ushort x = (ushort)rnd.Next(1, 63), y = (ushort)rnd.Next(1, x);
+        Console.WriteLine("x =" + x + " y =" + y);
+        Console.WriteLine("x =" + new RomanNumber(x) + " y =" + new RomanNumber(y));
+        Console.WriteLine("x + y = " + x + " + " + y + " = " + RomanNumber.Add(new RomanNumber(x), new RomanNumber(y)) + "(" + (x + y) + ")");
+        Console.WriteLine("x - y = " + x + " - " + y + " = " + RomanNumber.Sub(new RomanNumber(x), new RomanNumber(y)) + "(" + (x - y) + ")");
+        Console.WriteLine("x * y = " + x + " * " + y + " = " + RomanNumber.Mul(new RomanNumber(x), new RomanNumber(y)) + "(" + (x * y) + ")");
+        Console.WriteLine("x / y = " + x + " / " + y + " = " + RomanNumber.Div(new RomanNumber(x), new RomanNumber(y)) + "(" + (x / y) + ")");
+        Console.WriteLine();
+
+        ushort[] Arr = new ushort[5];
+        RomanNumber[] RomanArr = new RomanNumber[5];
+
+        Console.WriteLine("Unsorted arr: ");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Arr[i] = (ushort)(rnd.Next(1, 3999));
+            RomanArr[i] = new RomanNumber(Arr[i]);
+            Console.Write(Arr[i] + " ");
+        }
+        Console.WriteLine();
+        for (int i = 0; i < 5; i++)
+            Console.Write(RomanArr[i] + " ");
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Array.Sort(Arr);
+        Array.Sort(RomanArr);
+
+        Console.WriteLine("Sorted arr: ");
+
+        for (int i = 0; i < 5; i++)
+            Console.Write(Arr[i] + " ");
+        Console.WriteLine();
+        for (int i = 0; i < 5; i++)
+            Console.Write(RomanArr[i] + " ");
+        Console.WriteLine();
+    }
+}
+
---
+++ lab3Tests1/RomanNumberTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass()]
+    public class RomanNumberTests
+    {
+        [TestMethod()]
+        public void CloneTest()
+        {
+            RomanNumber test = new RomanNumber(24);
+            RomanNumber c_test = test.Clone() as RomanNumber;
+            Assert.IsNotNull(c_test);
+            Assert.AreEqual(c_test.ToString(), test.ToString());
+            
+        }
+
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            var test1 = new RomanNumber(34);
+            var test2 = new RomanNumber(34);
+            string expected = "0";
+            var c_test = test1.CompareTo(test2);
+            Assert.IsNotNull(c_test);
+            Assert.AreEqual(c_test.ToString(), expected);
+            Assert.ThrowsException<RomanNumberException>(() => test1.CompareTo(null as RomanNumber));
+        }
+
+        [TestMethod()]
+        public void RomanNumberTest()
+        {
+            RomanNumber test = new RomanNumber(55);
+            Assert.IsNotNull(test);
+            Assert.ThrowsException<RomanNumberException>(() => new RomanNumber(4000));
+            Assert.ThrowsException<RomanNumberException>(() => new RomanNumber(0));
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            RomanNumber test1_1 = new RomanNumber(55);
+            RomanNumber test1_2 = new RomanNumber(34);
+            string expected1 = "LXXXIX";
+
+            Assert.AreEqual(expected1, RomanNumber.Add(test1_1, test1_2).ToString());
+            RomanNumber test2_1 = new RomanNumber(3550);
+            RomanNumber test2_2 = new RomanNumber(650); 
+            Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Add(test2_1, test2_2));
+            Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Add(null as RomanNumber, test2_2));
+        }
+
+        [TestMethod()]
+        public void SubTest()
+        {
+            RomanNumber test1 = new RomanNumber(55);
+            RomanNumber test2 = new RomanNumber(34);
+            string expected = "XXI";
+
+            Assert.AreEqual(expected, RomanNumber.Sub(test1, test2).ToString());
+            Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Sub(test2, test1));
+            Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Sub(null as RomanNumber, test1));
+        }
+
+        [TestMethod()]
+        public void MulTest()
+        {
+            RomanNumber test1_1 = new RomanNumber(55);
+            RomanNumber test1_2 = new RomanNumber(34);
+            string expected = "MDCCCLXX";
+
+            Assert.AreEqual(expected, RomanNumber.Mul(test1_1, test1_2).ToString());
+
+            RomanNumber test2_1 = new RomanNumber(550);
+            RomanNumber test2_2 = new RomanNumber(32);
+            Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Mul(test2_1, test2_2));
+            Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Mul(null as RomanNumber, test2_2));
+        }
+
+        [TestMethod()]
+        public void DivTest()
+        {
+            RomanNumber test1 = new RomanNumber(55);
+            RomanNumber test2 = new RomanNumber(34);
+            string expected = "I";
+            Assert.AreEqual(expected, RomanNumber.Div(test1, test2).ToString());
+            Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Div(test2, test1));
+            Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Div(null as RomanNumber, test2));
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            RomanNumber test = new RomanNumber(3999);
+            string expected = "MMMCMXCIX";
+            Assert.AreEqual(expected, test.ToString());
+            Assert.ThrowsException<RomanNumberException>(() => new RomanNumber(5041));
+        }
+    }
+}
